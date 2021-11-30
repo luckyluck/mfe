2ff64c8e742d1036493de32223224bf7c18443ff
Alexandr Ketov <alexandr.ketov@doktor24.se>
2021-11-30 22:24:26 +0100

    Using Browser History in Isolation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { defaultHistory, onNavigate }) => {
+  const history = defaultHistory ?? createMemoryHistory();
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot, {});
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
