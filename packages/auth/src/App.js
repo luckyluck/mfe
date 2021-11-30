a238392aa1e0e8d27a33c557fd0e7e5d7184dced
Alexandr Ketov <alexandr.ketov@doktor24.se>
2021-11-30 22:38:04 +0100

    Adding Signin and Signup Forms

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
 });
@@ -12,6 +15,8 @@ export default ({ history }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
+            <Route path="/auth/signin" component={SignIn} />
+            <Route path="/auth/signup" component={SignUp} />
           </Switch>
         </Router>
       </StylesProvider>
