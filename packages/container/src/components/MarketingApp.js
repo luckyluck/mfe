5ff6a67cd2ade79ccb96ade2e5752c93b962cd68
Alexandr Ketov <alexandr.ketov@doktor24.se>
2021-11-30 22:10:06 +0100

    Syncing History Objects

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,14 +1,20 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
     mount(ref.current, {
-      onNavigate: () => {
-        console.log('The container noticed navigation in Marketing');
+      onNavigate: ({ pathname: nextPathName }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
       },
     });
   }, []);
